6ca9d6df1da8f1a4eaabff880cef083754506ae2
givenjun <152844483+givenjun@users.noreply.github.com>
2025-09-07 17:26:01 +0900

    Title: 원소 10개의 합, Time: 85ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250907/원소 10개의 합/sum-of-10-elements.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] str = br.readLine().split(" ");
+
+        int sum = 0;
+        for (int i = 0; i < str.length; i++) {
+            sum += Integer.parseInt(str[i]);
+        }
+
+        bw.write(sum+"");
+        bw.flush();
+        bw.close();
+        
+    }
+}
