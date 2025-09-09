9ad1bdaffbc57c8414c6f3bbe411f02261c10ed0
givenjun <152844483+givenjun@users.noreply.github.com>
2025-09-09 14:02:11 +0900

    Title: 점수대 파악하기, Time: 90ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250909/점수대 파악하기/find-out-the-score-range.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] str = br.readLine().split(" ");
+        int[] arr = new int[10];
+
+        for (String s : str) {
+            int num = Integer.parseInt(s);
+            int grade = num / 10;
+
+            if (grade == 0) continue;
+            arr[grade - 1]++;
+        }
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            bw.write((i + 1) * 10 + " - " + arr[i] + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
