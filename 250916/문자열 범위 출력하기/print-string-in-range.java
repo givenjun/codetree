14148d40825123659b93224c1a38836994c56bec
givenjun <152844483+givenjun@users.noreply.github.com>
2025-09-16 11:19:20 +0900

    Title: 문자열 범위 출력하기, Time: 92ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250916/문자열 범위 출력하기/print-string-in-range.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+        String sub = str.substring(2, 10);
+        
+        bw.write(sub);
+        bw.flush();
+        bw.close();
+
+    }
+}
