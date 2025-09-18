394b17d5dcb909333f36b15790e7cc5c8281aef1
givenjun <152844483+givenjun@users.noreply.github.com>
2025-09-18 14:56:27 +0900

    Title: 문자열 계속 지우기, Time: 89ms, Memory: 9MB, Status: Passed - Codetree

--- 250918/문자열 계속 지우기/keep-removing-string.java
+++ 250918/문자열 계속 지우기/keep-removing-string.java
@@ -10,18 +10,23 @@ public class Main {
         String part = br.readLine();
         int w_len = whole.length();
         int p_len = part.length();
+        int cnt = 0;
 
         for (int i = 0; i < w_len - p_len + 1; i++) {
+            // bw.write(i+"\n");
             String start = whole.substring(0, i);
             String comp = whole.substring(i, i + p_len);
             String end = whole.substring(i + p_len);
 
+            // bw.write(comp + " " + part + "\n");
             if (comp.equals(part)) {
                 whole = start + end;
-                i = 0;
+                i = -1;
                 w_len = whole.length();
+                cnt++;
             }
         }
+        // bw.write(cnt+" ");
         bw.write(whole);
         bw.flush();
         bw.close();
