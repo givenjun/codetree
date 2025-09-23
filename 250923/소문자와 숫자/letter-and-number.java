17a86ba3bdf082024ba7c663f12cd818fb2dcd4c
givenjun <152844483+givenjun@users.noreply.github.com>
2025-09-23 11:22:31 +0900

    Title: 소문자와 숫자, Time: 86ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250923/소문자와 숫자/letter-and-number.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+        String res = "";
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')) res += c;
+            else if (c >= 'A' && c <= 'Z') res += (char)(c - 'A' + 'a');
+        }
+        bw.write(res);
+        bw.flush();
+        bw.close();
+    }
+}
