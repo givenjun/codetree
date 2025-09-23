f1536395adca6809503884234b61e3abc66e1fd7
givenjun <152844483+givenjun@users.noreply.github.com>
2025-09-23 14:42:09 +0900

    Title: 일치하는 문자열의 수, Time: 86ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250923/일치하는 문자열의 수/num-of-correct-string.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] str = br.readLine().split(" ");
+        int N = Integer.parseInt(str[0]);
+        String s = str[1];
+        int cnt = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (s.equals(br.readLine())) cnt++;
+        }
+        bw.write(cnt + "");
+        bw.flush();
+        bw.close();
+    }
+}
