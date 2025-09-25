811604f9ff42593870bb3ed946e5be3ce3d0ec98
givenjun <152844483+givenjun@users.noreply.github.com>
2025-09-25 14:42:25 +0900

    Title: 최소공배수 구하기, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

--- 250925/최소공배수 구하기/find-the-least-common-multiple.java
+++ 250925/최소공배수 구하기/find-the-least-common-multiple.java
@@ -18,6 +18,22 @@ public class Main {
             int val = n * i;
             sb1.append(val + " ");
         }
+        for (int i = 1; i <= n; i++) {
+            int val = m * i;
+            sb2.append(val + " ");
+        }
+        String[] n_mul = sb1.toString().split(" ");
+        String[] m_mul = sb2.toString().split(" ");
+        for (int i = 0; i < n_mul.length; i++) {
+            for (int j = 0; j < m_mul.length; j++) {
+                if (n_mul[i].equals(m_mul[j])) {
+                    if (Integer.parseInt(n_mul[i]) < Integer.parseInt(m_mul[j])) break;
+                    mul = Integer.parseInt(n_mul[i]);
+                    break;
+                }
+                if (mul != 0) break;
+            }
+        }
         return mul;
     }
 }
