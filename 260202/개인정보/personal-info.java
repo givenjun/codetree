84d856144d59d095934d4bf3d8bb87556fd5ea4e
givenjun <152844483+givenjun@users.noreply.github.com>
2026-02-02 16:04:16 +0900

    Title: 개인정보, Time: 154ms, Memory: 12MB, Status: Passed - Codetree

---
+++ 260202/개인정보/personal-info.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+class Person implements Comparable<Person> {
+    String name;
+    int height;
+    double weight;
+    boolean nameSort;
+    public Person(String name, int height, double weight, boolean nameSort) {
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+        this.nameSort = nameSort;
+    }
+    public void print() {
+        System.out.println(name + " " + height + " " + weight);
+    }
+    @Override
+    public int compareTo(Person p) {
+        if (nameSort) {
+            return p.height - this.height;
+        }
+        return this.name.compareTo(p.name);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 5;
+        String[] names = new String[n];
+        int[] heights = new int[n];
+        double[] weights = new double[n];
+        Person[] person = new Person[n];
+        Person[] p = new Person[n];
+        for (int i = 0; i < n; i++) {
+            names[i] = sc.next();
+            heights[i] = sc.nextInt();
+            weights[i] = sc.nextDouble();
+            person[i] = new Person(names[i], heights[i], weights[i], false);
+            p[i] = new Person(names[i], heights[i], weights[i], true);
+        }
+        // Please write your code here.
+        Arrays.sort(person);
+        System.out.println("name");
+        for(Person personE : person) {
+            personE.print();
+        }
+
+        System.out.println();
+        Arrays.sort(p);
+        System.out.println("height");
+        for(Person pE : p) {
+            pE.print();
+        }
+    }
+}
