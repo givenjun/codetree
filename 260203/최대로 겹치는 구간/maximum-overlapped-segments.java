0da717dc8a717c56fc1bada0b79b05781a373d26
givenjun <152844483+givenjun@users.noreply.github.com>
2026-02-03 14:28:14 +0900

    Title: 최대로 겹치는 구간, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260203/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] list = new int[201];
+
+        for (int i = 0; i < n; i++) {
+            String[] se = br.readLine().split(" ");
+            int s = Integer.parseInt(se[0]);
+            int e = Integer.parseInt(se[1]);
+
+            for (int j = s; j < e; j++) {
+                list[j]++;
+            }
+        }
+        Arrays.sort(list);
+        System.out.println(list[200]);
+
+    }
+}
