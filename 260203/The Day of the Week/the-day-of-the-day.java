f00d2a6f29d7e174abe2f8ecb88fced75dfdaff7
givenjun <152844483+givenjun@users.noreply.github.com>
2026-02-03 10:51:47 +0900

    Title: The Day of the Week, Time: 129ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260203/The Day of the Week/the-day-of-the-day.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+        String A = sc.next();
+        // Please write your code here.
+        int[] month = new int[]{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] day = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+
+        int total1 = 0;
+        int total2 = 0;
+
+        for (int i = 1; i < m1; i++) {
+            total1 += month[i];
+        }
+        total1 += d1;
+
+        for (int i = 1; i < m2; i++) {
+            total2 += month[i];
+        }
+        total2 += d2;
+
+        int dif = total2 - total1 - Arrays.asList(day).indexOf(A);
+        System.out.println(dif / 7 + 1);
+    }
+}
