176c7ffcafd50d2add048c9def0486b05cf13e5f
givenjun <152844483+givenjun@users.noreply.github.com>
2026-02-04 16:36:32 +0900

    Title: 사각형의 총 넓이 2, Time: 88ms, Memory: 9MB, Status: Runtime Error - Codetree

---
+++ 260204/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        boolean[][] plane = new boolean[201][201];
+
+        for (int i = 0; i < n; i++) {
+            String[] points = br.readLine().split(" ");
+            int x1 = Integer.parseInt(points[0]);
+            int y1 = Integer.parseInt(points[1]);
+            int x2 = Integer.parseInt(points[2]);
+            int y2 = Integer.parseInt(points[3]);
+
+            for (int j = y1; j < y2; j++) {
+                for (int k = x1; k < x2; k++) {
+                    plane[j][k] = true;
+                }
+            }
+        }
+
+        int size = 0;
+        for (int i = 0; i < 201; i++) {
+            for (int j = 0; j < 201; j++) {
+                if (plane[i][j]) size++;
+            }
+        }
+
+        System.out.println(size);
+    }
+}
