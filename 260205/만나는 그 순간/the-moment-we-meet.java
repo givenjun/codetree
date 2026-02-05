13d9b822bc76f8207f471748cd7b4fd6f4ef1eb8
givenjun <152844483+givenjun@users.noreply.github.com>
2026-02-05 20:07:26 +0900

    Title: 만나는 그 순간, Time: 124ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260205/만나는 그 순간/the-moment-we-meet.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int size = (n >= m) ? n : m;
+        size *= 1000;
+        int[][] chk = new int[2][size];
+
+        int val1 = 0, val2 = 0, t1 = 0, t2 = 0;
+        for (int i = 0; i < n; i++) {
+            String direction = sc.next();
+            int time = sc.nextInt();
+
+            for (int j = 0; j < time; j++) {
+                if (direction.equals("R")) {
+                    val1++;
+                }
+                else {
+                    val1--;
+                }
+                t1++;
+                chk[0][t1] = val1;
+            } 
+        }
+        for (int i = 0; i < m; i++) {
+            String direction = sc.next();
+            int time = sc.nextInt();
+
+            for (int j = 0; j < time; j++) {
+                if (direction.equals("R")) {
+                    val2++;
+                }
+                else {
+                    val2--;
+                }
+                t2++;
+                chk[1][t2] = val2;
+            } 
+        }
+
+        int res = -1;
+        for (int i = 1; i < size; i++) {
+            // System.out.println(chk[0][i] + " " + chk[1][i]);
+            if (chk[0][i] == chk[1][i]) {
+                res = i;
+                break;
+            }
+        }
+        System.out.print(res);
+
+    }
+}
