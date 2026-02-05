4a26b7e1ef1190bc40cb321e7f2d79dbef8960e3
givenjun <152844483+givenjun@users.noreply.github.com>
2026-02-05 10:37:43 +0900

    Title: 연속되는 수 3, Time: 124ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 260205/연속되는 수 3/continuous-number3.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int[] chk = new int[n / 2];
+        int pos = 0, cnt = 1;
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            chk[pos] = cnt;
+            if (i >= 1) {
+                if ((arr[i - 1] * arr[i]) < 0) {
+                    cnt = 1;
+                    pos++;
+                }
+                else if (arr[i - 1] > 0 && arr[i] > 0) {
+                    cnt++;
+                    chk[pos] = cnt;
+                }
+                else if (arr[i - 1] < 0 && arr[i] < 0) {
+                    cnt++;
+                    chk[pos] = cnt;
+                }
+            }
+        }
+        int max = 0;
+        for (int c : chk) {
+            if (c > max) max = c;
+        }
+        System.out.print(max);
+    }
+}
