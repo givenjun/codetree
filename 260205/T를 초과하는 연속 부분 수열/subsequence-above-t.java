9a5e4313b8c817118347e5a6edcc4c7a8ae1ecbf
givenjun <152844483+givenjun@users.noreply.github.com>
2026-02-05 11:16:53 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 154ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260205/T를 초과하는 연속 부분 수열/subsequence-above-t.java
+++ 260205/T를 초과하는 연속 부분 수열/subsequence-above-t.java
@@ -6,7 +6,7 @@ public class Main {
         int n = sc.nextInt();
         int t = sc.nextInt();
         int[] arr = new int[n];
-        int[] chk = new int[n / 2 + 1];
+        int[] chk = new int[n];
         for (int i = 0; i < n; i++)
             arr[i] = sc.nextInt();
         // Please write your code here.
