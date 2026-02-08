9151361375ba4294b7bbf54ea31932829e79d846
givenjun <152844483+givenjun@users.noreply.github.com>
2026-02-08 17:49:25 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 260208/빙빙 돌며 사각형 채우기/snail-alphabet-square.java
+++ 260208/빙빙 돌며 사각형 채우기/snail-alphabet-square.java
@@ -15,34 +15,13 @@ public class Main {
         char alpha = 'A';
         matrix[0][0] = alpha;
 
-        int x = 0, y = 0, d = 0;
-        int[] dx = new int[]{1, 0, -1, 0};
-        int[] dy = new int[]{0, 1, 0, -1};
-
         int cnt = 0;
         while (cnt == n * m) {
             cnt++;
             aplha++;
-            if (alpha = 'Z' + 1) {
-                alpha = 'A';
-            }
-            int nextY = y + dy[d];
-            int nextX = x + dx[d];
-            if (!inRange(nextY, nextX) || matrix[nextY][nextX] != '\0') {
-                d = (d + 1) % 4;
-            }
-            x += dx[d];
-            y += dy[d];
-            matrix[y][x] = alpha;
         }
         
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                bw.write(matrix[i][j] + " ");
-            }
-            bw.newLine();
-        }
-        bw.flush();
-        bw.close();
+
+
     }
 }
