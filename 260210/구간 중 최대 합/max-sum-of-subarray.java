ec67b0e545b107ccf6152e11fb0dca7aaf1e37e9
givenjun <152844483+givenjun@users.noreply.github.com>
2026-02-10 15:43:28 +0900

    Title: 구간 중 최대 합, Time: 91ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 260210/구간 중 최대 합/max-sum-of-subarray.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] nk = br.readLine().split(" ");
+        int n = Integer.parseInt(nk[0]);
+        int k = Integer.parseInt(nk[1]);
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int max = 0;
+        for (int i = 0; i < n - k; i++) {
+            int sum = 0;
+            for (int j = i; j < i + k; j++) {
+                sum += arr[j];
+            }
+            if (sum > max) max = sum;
+        }
+        System.out.print(max);
+    }
+}
