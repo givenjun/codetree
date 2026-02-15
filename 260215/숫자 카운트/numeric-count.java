8be52cc3ba34198e6b32f3bbe12eba05e2a06ebd
givenjun <152844483+givenjun@users.noreply.github.com>
2026-02-15 17:41:47 +0900

    Title: 숫자 카운트, Time: 90ms, Memory: 9MB, Status: Passed - Codetree

--- 260215/숫자 카운트/numeric-count.java
+++ 260215/숫자 카운트/numeric-count.java
@@ -6,22 +6,49 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int[] firstNum = new int[10];
-        int[] firstNum = new int[10];
+        int[] numQ = new int[10];
+        int[] strikeQ = new int[10];
+        int[] ballQ = new int[10];
 
-        String[] str = new String[4];
         for (int i = 0; i < n; i++) {
-            str[i] = br.readLine();
-            String[] b = str[i].split(" ");
-            int first = b[0].charAt(0);
-            int second = b[0].charAt(1);
-            int third = b[0].charAt(2);
-
-            int strike = Integer.parseInt(b[1]);
-            int ball = Integer.parseInt(b[2]);
-
+            String[] str = br.readLine().split(" ");
+            numQ[i] = Integer.parseInt(str[0]);
+            strikeQ[i] = Integer.parseInt(str[1]);
+            ballQ[i] = Integer.parseInt(str[2]);
         }
 
+        int cnt = 0;
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                for (int k = 1; k <= 9; k++) {
+                    if (i == j || j == k || k == i) continue;
+                    
+                    boolean cond = true;
+
+                    for (int q = 0; q < n; q++) {
+                        int a = numQ[q] / 100;
+                        int b = numQ[q] / 10 % 10;
+                        int c = numQ[q] % 10;
+
+                        int strike = 0, ball = 0;
+                        if (a == i) strike++;
+                        if (b == j) strike++;
+                        if (c == k) strike++;
+                        if (a == j || a == k) ball++;
+                        if (b == i || b == k) ball++;
+                        if (c == i || c == j) ball++;
+
+                        if (strike != strikeQ[q] || ball != ballQ[q]) {
+                            cond = false;
+                            break;
+                        }
+                    }
+
+                    if (cond) cnt++;                
+                }
+            }
+        }
+        System.out.print(cnt);
 
     }
 }
