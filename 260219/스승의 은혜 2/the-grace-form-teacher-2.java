e96d9caf530edffb2b5a5f01f73b2786e4da5b12
givenjun <152844483+givenjun@users.noreply.github.com>
2026-02-19 17:05:46 +0900

    Title: 스승의 은혜 2, Time: 87ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 260219/스승의 은혜 2/the-grace-form-teacher-2.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int[] students = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            students[i] = Integer.parseInt(br.readLine());
+        }
+
+        int maxCnt = 0;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            int cnt = 0;
+            for (int j = 0; j < n; j++) {
+                
+                if (i == j) sum += students[j] / 2;
+                else sum += students[j];
+
+                if (sum >= b) {
+                    break;
+                }
+
+                cnt++;
+            
+                // System.out.print(sum + " ");
+            }
+            // System.out.println();
+            maxCnt = Math.max(maxCnt, cnt);
+        }
+
+        System.out.print(maxCnt);
+    }
+}
